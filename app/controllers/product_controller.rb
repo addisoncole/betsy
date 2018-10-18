fb380f139174c58c3a898d8a164bf8f18fe1f6f4
Naheed Arang <arang.na@gmail.com>
2018-10-18 11:41:33 -0700

    add restful routes for user and product controller

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,2 +1,38 @@
 class ProductController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render_404 unless @product
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @product.destroy
+    flash[:status] = :success
+    flash[:result_text] = "Successfully destroyed #{@product.singularize} #{@product.id}"
+    redirect_to root_path
+  end
+
+
+  private
+  def product_params
+    params.require(:product).permit(:category, :name, :price, :quantity)
+  end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,37 @@
 class UserController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render_404 unless @user
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @user.destroy
+    flash[:status] = :success
+    flash[:result_text] = "Successfully destroyed #{@user.singularize} #{@user.id}"
+    redirect_to root_path
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:category, :name, :price, :quantity)
+  end
 end
