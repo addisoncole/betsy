17d5aebfdc4e009a7a0aced5f22da97cb9652e7b
Addison Cole <addison.cole85@gmail.com>
2018-10-18 12:43:40 -0700

    product seed data created

---
+++ .fetchtest/generate_product_seeds.rb
@@ -0,0 +1,46 @@
+require 'faker'
+require 'date'
+require 'csv'
+
+# we already provide a filled out media_seeds.csv file, but feel free to
+# run this script in order to replace it and generate a new one
+# run using the command:
+# $ ruby .fetchtest/generate_product_seeds.rb
+# if satisfied with this new media_seeds.csv file, recreate the db with:
+# $ rails db:reset
+# doesn't currently check for if titles are unique against each other
+
+CSV.open('.fetchtest/product_seeds.csv', "w", :write_headers=> true,
+  :headers => ["name", "category", "quantity", "price", "description"]) do |csv|
+
+    25.times do |i|
+      category = %w(Plants Housewares Fashion Beauty Lifestyle).sample
+      if category == "Plants"
+        name = Faker::Ancient.primordial + " " + Faker::Zelda.item
+        quantity = rand(25)
+        price = 6.66
+      end
+      if category == "Housewares"
+        name = Faker::Robin.quote + " " + Faker::GameOfThrones.house
+        quantity = rand(25)
+        price = 6.66
+      end
+      if category == "Fashion"
+        name = Faker::Nation.capital_city + " " + Faker::TwinPeaks.location
+        quantity = rand(25)
+        price = 6.66
+      end
+      if category == "Beauty"
+        name = Faker::Ancient.primordial + " " + Faker::Dessert.flavor
+        quantity = rand(25)
+        price = 6.66
+      end
+      if category == "Lifestyle"
+        name = Faker::Nation.national_sport + " " +  Faker::Dog.meme_phrase
+        quantity = rand(25)
+        price = 6.66
+      end
+
+      csv << [category, name, quantity, price]
+    end
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+PRODUCT_FILE = Rails.root.join('db', 'seed_data', 'products.csv')
+puts "Loading raw driver data from #{PRODUCT_FILE}"
+
+product_failures = []
+CSV.foreach(PRODUCT_FILE, :headers => true) do |row|
+  product = Product.new
+  product.category = row['category']
+  product.name = row['name']
+  product.quantity = row['quantity']
+  product.price = row['price']
+  successful = product.save
+  if !successful
+    product_failures << product
+    puts "Failed to save product: #{product.inspect}"
+  else
+    puts "Created product: #{product.inspect}"
+  end
+end
+
+puts "Added #{Product.count} product records"
+puts "#{product_failures.length} product failed to save"
+
+puts "Manually resetting PK sequence on each table"
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+puts "done"
