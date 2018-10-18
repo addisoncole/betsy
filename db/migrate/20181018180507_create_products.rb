4920fc4e0ea93283fadfe341489fcb99616a7853
Addison Cole <addison.cole85@gmail.com>
2018-10-18 11:14:25 -0700

    fixed model initialization

--- db/migrate/20181018180507_create_products.rb
+++ db/migrate/20181018180507_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.name :string
+      t.string :name
       t.string :category
       t.integer :quantity
       t.decimal :price
