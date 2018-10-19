8cbc268168690ac42b53dbbd30b6ec2de1bf174d
Addison Cole <addison.cole85@gmail.com>
2018-10-19 14:32:26 -0700

    created model validation testing for products.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,9 +1,70 @@
 require "test_helper"
 
 describe Product do
-  let(:product) { Product.new }
 
-  it "must be valid" do
-    value(product).must_be :valid?
+  # Product must belong to a User
+  describe 'validations' do
+    before do
+      # Arrange
+      @product = Product.new(name: 'Avocado Toast', category: "Lifestyle", quantity: 4, price: 9.99, image: 'https://placekitten.com/200/200')
+    end
+
+    it 'is valid when all fields are present' do
+      # Act
+      result = @product.valid?
+
+      # Assert
+      expect(result).must_equal true
+    end
+
+    it 'it is invalid if no name for the product is present' do
+      # Act
+      @product.name = nil
+      result = @product.valid?
+
+      # Assert
+      expect(result).must_equal true
+    end
+
+    it 'it is invalid if another product has the same name' do
+      # Act
+      @product2 = Product.new(name: 'Avocado Toast', category: "Lifestyle", quantity: 4, price: 9.99, image: 'https://placekitten.com/200/200')
+      result = @product2.valid?
+
+      # Assert
+      expect(result).must_equal false
+    end
+    it 'it must have a price' do
+      # Act
+      @product.price = nil
+      result = @product.valid?
+
+      # Assert
+      expect(result).must_equal false
+    end
+    it 'the price must be a number' do
+      # Act
+      @product.price = "bumblebeetuna"
+      result = @product.valid?
+
+      # Assert
+      expect(result).must_equal false
+    end
+    it 'price must be greater than zero' do
+      # Act
+      @product.price = 0.0
+      result = @product.valid?
+
+      # Assert
+      expect(result).must_equal false
+    end
+    it 'price cannot be a negative number' do
+      # Act
+      @product.price = -9.99
+      result = @product.valid?
+
+      # Assert
+      expect(result).must_equal false
+    end
   end
 end
