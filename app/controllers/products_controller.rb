1099be25d9419ec077c2b154dd443881c68577fa
Addison Cole <addison.cole85@gmail.com>
2018-10-20 18:10:46 -0700

    added logic to products controller to sort data by catagories. application nav links for each product category now passes a param of the category, links to products path, and the controller sorts if that param is present. this all shows on the products_path/products index view.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,11 @@ class ProductsController < ApplicationController
   before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   def index
-    @products = Product.all
+    if params[:category]
+      @products = Product.where(:category => params[:category])
+    else
+      @products = Product.all
+    end
   end
 
   def new
