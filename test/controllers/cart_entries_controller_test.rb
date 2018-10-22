fc9c5c5cbfed1d9e22bc5b891d7b0932c01f21d0
Karis Kim <ouniiq@hotmail.com>
2018-10-22 13:42:26 -0700

    test case, ignore this commit

--- test/controllers/cart_entries_controller_test.rb
+++ test/controllers/cart_entries_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 describe CartEntriesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it "must be a real test" do
+    # flunk "Need real tests"
+  end
 end
