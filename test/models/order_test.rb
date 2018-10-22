1c7e9cae5d4400b39fdea7ac584d74a7e90281c7
Addison Cole <addison.cole85@gmail.com>
2018-10-21 17:39:30 -0700

    Revert "test prep for models of cart entry, order, reviews and user"
    This reverts commit 3bdc639823e6da335f7b846837729568ab426bef.

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,18 +1,5 @@
 require "test_helper"
 
-# t.datetime "created_at", null: false
-# t.datetime "updated_at", null: false
-# t.integer "card_number"
-# t.string "card_expiration"
-# t.integer "CVV"
-# t.integer "billing_zip_code"
-# t.string "shipping_address"
-# t.string "email"
-# t.string "address"
-# t.string "status"
-
-# An Order must have one or more Order Items
-
 describe Order do
   let(:order) { Order.new }
 
