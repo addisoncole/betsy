8c8f40987b1b0cf7bdcea6392122723ae97eb76e
Addison Cole <addison.cole85@gmail.com>
2018-10-23 13:07:44 -0700

    added logic to controller that redirects a guest user to the root page and flash an error if they try to access the users page or any user's page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,12 @@ class UsersController < ApplicationController
   before_action :find_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @users = User.all
+    if User.find_by(id: session[:user_id])
+      @users = User.all
+    else
+      flash[:error] = "Must be logged in to do that."
+      redirect_to root_path
+    end
   end
 
   def new
@@ -14,8 +19,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id])
-    render_404 unless @user
+    if User.find_by(id: session[:user_id])
+      @user = User.find_by(id: params[:id])
+      render_404 unless @user
+    else
+      flash[:error] = "Must be logged in to do that."
+      redirect_to root_path
+    end
   end
 
   def edit
