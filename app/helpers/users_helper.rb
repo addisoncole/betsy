048dd0144e55da4400b58973aea4f81f8d8bf65c
Addison Cole <addison.cole85@gmail.com>
2018-10-23 23:03:43 -0700

    added two partials to show a user dashbord and or a user pop up/storefront. added a couple of view helpers to hide buttons from non authorized users.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,20 @@
 module UsersHelper
 
-  def display_edit_button?(user)
+  def display_edit_pop_up_button?(user)
     if @user == User.find_by(id: session[:user_id])
        (link_to "Edit yr pop-up", edit_user_path(user_id: user)).html_safe
+    end
+  end
+
+  def display_edit_button?(user)
+    if @user == User.find_by(id: session[:user_id])
+       (link_to "Edit yr profile", edit_user_path(user_id: user)).html_safe
+    end
+  end
+
+  def display_sign_up_button?(user)
+    if @user == User.find_by(id: session[:user_id])
+       (link_to "Start yr pop up", edit_user_path(user_id: user)).html_safe
     end
   end
 
@@ -12,13 +24,10 @@ module UsersHelper
    end
  end
 
-  def display_store_name_or_users_name(user)
-    if @user.store_name
-      return @user.store_name
-    else
-      return @user.name.downcase
-    end
-  end
+def get_average_rating(user)
+  products = user.products
+  binding.pry
+end
 
   def display_users_join_date(date)
     ("<span>" + "Member since: " + date.strftime("%b %d") + ", 2k" +  date.strftime("%y") + "</span>").html_safe
