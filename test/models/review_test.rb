5bb3bfbd9485d1dcfaa0c7ea6c319f41d9c2fede
Karis Kim <ouniiq@hotmail.com>
2018-10-24 09:54:19 -0700

    started review model testing

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,14 +1,7 @@
 require "test_helper"
 
-# t.string "name"
-# t.string "username"
-# t.string "email"
-# t.boolean "merchant", default: false
-
-# describe Review do
-#   let(:review) { Review.new }
-#
-#   it "must be valid" do
-#     value(review).must_be :valid?
-#   end
-# end
+describe Review do
+  it "must be valid" do
+    reviews(:first).must_be :valid?
+  end
+end
