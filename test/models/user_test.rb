3142ca587e93afccd3655aa0424162a3816e2fc4
Karis Kim <ouniiq@hotmail.com>
2018-10-24 14:00:16 -0700

    added validation tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,5 +11,20 @@ describe User do
     before do
       @user = User.first
     end
+
+    it "must have an email" do
+      @user.must_be :valid?
+    end
+
+    it "does not accept data without an email" do
+      @user.email = nil
+
+      expect(@user.valid?).must_equal false
+      expect ( @user.errors.messages ).must_include :email
+    end
+  end
+
+  describe "relations" do
+
   end
 end
