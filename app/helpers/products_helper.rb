5d72a28af839797d58196cd764664a1f41934154
Addison Cole <addison.cole85@gmail.com>
2018-10-23 23:15:33 -0700

    added view helpers to products show page to hide delete and edit buttons from users who are not that product's merchant.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -5,17 +5,28 @@ module ProductsHelper
       product.quantity
     else
       return ("<span class='sell-out'> 🔥 SELL OUT 🔥
- </span>").html_safe
+        </span>").html_safe
+      end
     end
-  end
 
-  def display_add_to_cart_or_sold_out(product)
-    if product.quantity > 0
-       (button_to "Add to cart", cart_entries_path(product_id: @product)).html_safe
-    else
-      ("<button class='sold-out'>" + "SOLD OUT" + "</button>").html_safe
+    def display_add_to_cart_or_sold_out(product)
+      if product.quantity > 0
+        (button_to "Add to cart", cart_entries_path(product_id: @product)).html_safe
+      else
+        ("<button class='sold-out'>" + "SOLD OUT" + "</button>").html_safe
+      end
     end
-  end
+
+    def display_edit_product_button?(product)
+      if product.user == User.find_by(id: session[:user_id])
+        (link_to "Edit", edit_product_path(product.id)).html_safe
+      end
+    end
+
+    def display_delete_product_button?(product)
+      if product.user == User.find_by(id: session[:user_id])
+        (link_to "Delete", product_path, method: :delete, data: {confirm: "Delete  #{@product.name}?"}).html_safe
+      end
+    end
 end
-
-# <button></button>
+    # <button></button>
