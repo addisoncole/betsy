e81c341bf8d3ee3e69f2237f5f4aa24529162825
Karis Kim <ouniiq@hotmail.com>
2018-10-24 11:20:02 -0700

    passed relation test for review belonging to both user n product

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -4,4 +4,16 @@ describe Review do
   it "must be valid" do
     reviews(:first).must_be :valid?
   end
+
+  describe 'relations' do
+
+    it "belongs to a product" do
+      expect(reviews(:first).product).must_be_instance_of Product
+    end
+
+    it "belongs to a user" do
+      expect(reviews(:first).user).must_be_instance_of User
+    end
+
+  end
 end
