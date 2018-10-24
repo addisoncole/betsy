66644e4a3d548c89575f22ed693b29a6c0d8ec0a
Addison Cole <addison.cole85@gmail.com>
2018-10-23 22:32:57 -0700

    pulled merchant and storefront info on user show page and put into partial, will move user dashboard into a partial and render both on a user's show page.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,15 @@ module UsersHelper
 
   def display_edit_button?(user)
     if @user == User.find_by(id: session[:user_id])
-       (link_to "Edit", edit_user_path(user_id: user)).html_safe
+       (link_to "Edit yr pop-up", edit_user_path(user_id: user)).html_safe
     end
   end
+
+ def display_add_new_product?(user)
+   if @user.merchant
+     (link_to "Add new swag to pop-up", new_product_path).html_safe
+   end
+ end
 
   def display_store_name_or_users_name(user)
     if @user.store_name
@@ -12,6 +18,10 @@ module UsersHelper
     else
       return @user.name.downcase
     end
+  end
+
+  def display_users_join_date(date)
+    ("<span>" + "Member since: " + date.strftime("%b %d") + ", 2k" +  date.strftime("%y") + "</span>").html_safe
   end
 
 end
