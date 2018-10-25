1821231ef886a95c1f5c670165f97cb280921bec
Naheed Arang <arang.na@gmail.com>
2018-10-25 15:09:38 -0700

    order model testing

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,17 +1,16 @@
 class Order < ApplicationRecord
   has_many :cart_entries, dependent: :destroy
-  validate :card_number, :presence, if: Proc.new { |a| a.status == "paid" }
-  validates :card_number, :numericality => { :only_interger => true, :must_equal => 16 }
-  validate :card_expiration, :presence, if: Proc.new { |a| a.status == "paid" }
-  validate :CVV, :presence, if: Proc.new { |a| a.status == "paid" }
-  validates :CVV, :numericality => { :only_interger => true, :must_equal => 3 }
-  validate :billing_address, :presence, if: Proc.new { |a| a.status == "paid" }
-  validate :billing_zip_code, :presence, if: Proc.new { |a| a.status == "paid" }
-  validates :billing_zip_code, :numericality => { :only_interger => true, :must_equal => 5 }
-  validate :email, :presence, if: Proc.new { |a| a.status == "paid" }
-  validate :shipping_address, :presence, if: Proc.new { |a| a.status == "paid" }
+  validates :card_number, presence: true, length: { is: 16 }, :numericality => { :only_interger => true }, if: Proc.new { |a| a.status != "pending" }
+  validates :card_expiration, presence: true, if: Proc.new { |a| a.status != "pending" }
+  validates :CVV, presence: true, length: { is: 3 }, :numericality => { :only_interger => true }, if: Proc.new { |a| a.status != "pending" }
+  validates :billing_address, presence: true, if: Proc.new { |a| a.status != "pending" }
+  validates :billing_zip_code, presence: true, length: { is: 5 }, :numericality => { :only_interger => true }, if: Proc.new { |a| a.status != "pending" }
+  validates :email, presence: true, if: Proc.new { |a| a.status != "pending" }
+  validates :shipping_address, presence: true, if: Proc.new { |a| a.status != "pending" }
+  validates :status, presence: true
+  validates :name, presence: true, if: Proc.new { |a| a.status != "pending" }
 
-  def add_product(product, order_id)
+  def self.add_product(product, order_id)
     current_product = CartEntry.find_by(product_id: product.id, order_id: order_id)
     if current_product
       current_product.increment(:quantity)
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -3,127 +3,102 @@ require 'pry'
 
 describe Order do
   describe "validations" do
-    it "is valid when status is pending and no feilds are present" do
+    it "is valid when status is pending and no fields are present" do
       @order = Order.new(status: "pending")
-      binding.pry
       @order.valid?.must_equal true
     end
-    it "is valid when status is paid and all feilds are present" do
-      @order = Order.new(status: "paid", card_number: "2222222222222222", card_expiration: "10/12", CVV: "123", billing_zip_code: "12345", shipping_address: "fff", billing_address: "fff", email: "fff", name: "fff")
+    it "is valid when status is paid and all fields are present" do
+      @order = Order.new(status: "paid", card_number: 2222222222222222, card_expiration: "10/12", CVV: 123, billing_zip_code: 12345, shipping_address: "fff", billing_address: "fff", email: "fff", name: "fff")
       @order.valid?.must_equal true
+    end
+    it "is not valid when status is paid and no fields are present" do
+      @order = Order.new(status: "paid", card_number: nil, card_expiration: nil, CVV: nil, billing_zip_code: nil, shipping_address: nil, billing_address: nil, email: nil, name: nil)
+      @order.valid?.must_equal false
+    end
+    it "requires a card number when status is paid" do
+      @order = Order.new(status: "paid", card_expiration: "10/12", CVV: 123, billing_zip_code: 12345, shipping_address: "fff", billing_address: "fff", email: "fff", name: "fff")
+      @order.valid?.must_equal false
+      @order.errors.messages.must_include :card_number
+    end
+    it "requires a card expiration when status is paid" do
+      @order = Order.new(status: "paid", card_number: 2222222222222222, CVV: 123, billing_zip_code: 12345, shipping_address: "fff", billing_address: "fff", email: "fff", name: "fff")
+      @order.valid?.must_equal false
+      @order.errors.messages.must_include :card_expiration
+    end
+    it "requires a CVV when status is paid" do
+      @order = Order.new(status: "paid", card_number: 2222222222222222, card_expiration: "10/12", billing_zip_code: 12345, shipping_address: "fff", billing_address: "fff", email: "fff", name: "fff")
+      @order.valid?.must_equal false
+      @order.errors.messages.must_include :CVV
+    end
+    it "requires a billing zip code when status is paid" do
+      @order = Order.new(status: "paid", card_number: 2222222222222222, card_expiration: "10/12", CVV: 123, shipping_address: "fff", billing_address: "fff", email: "fff", name: "fff")
+      @order.valid?.must_equal false
+      @order.errors.messages.must_include :billing_zip_code
+    end
+    it "requires a shipping_address when status is paid" do
+      @order = Order.new(status: "paid", card_number: 2222222222222222, card_expiration: "10/12", CVV: 123, billing_zip_code: 12345, billing_address: "fff", email: "fff", name: "fff")
+      @order.valid?.must_equal false
+      @order.errors.messages.must_include :shipping_address
+    end
+    it "requires a billing_address when status is paid" do
+      @order = Order.new(status: "paid", card_number: 2222222222222222, card_expiration: "10/12", CVV: 123, billing_zip_code: 12345, shipping_address: "fff", email: "fff", name: "fff")
+      @order.valid?.must_equal false
+      @order.errors.messages.must_include :billing_address
+    end
+    it "requires an email when status is paid" do
+      @order = Order.new(status: "paid", card_number: 2222222222222222, card_expiration: "10/12", CVV: 123, billing_zip_code: 12345, shipping_address: "fff", billing_address: "fff", name: "fff")
+      @order.valid?.must_equal false
+      @order.errors.messages.must_include :email
+    end
+    it "requires a name when status is paid" do
+      @order = Order.new(status: "paid", card_number: 2222222222222222, card_expiration: "10/12", CVV: 123, billing_zip_code: 12345, shipping_address: "fff", billing_address: "fff", email: "fff")
+      @order.valid?.must_equal false
+      @order.errors.messages.must_include :name
+    end
+    it "card number must be of type bigint" do
+      @order = Order.new(status: "paid", card_number: "asdfghjklasdfghj", card_expiration: "10/12", CVV: 123, billing_zip_code: 12345, shipping_address: "fff", billing_address: "fff", email: "fff", name: "fff")
+      @order.valid?.must_equal false
+      @order.errors.messages.must_include :card_number
+    end
+    it "card number must be 16 digits" do
+      @order = Order.new(status: "paid", card_number: 12345678901, card_expiration: "10/12", CVV: 123, billing_zip_code: 12345, shipping_address: "fff", billing_address: "fff", email: "fff", name: "fff")
+      @order.valid?.must_equal false
+      @order.errors.messages.must_include :card_number
+    end
+    it "CVV must be of type integer" do
+      @order = Order.new(status: "paid", card_number: 2222222222222222, card_expiration: "10/12", CVV: "asd", billing_zip_code: 12345, shipping_address: "fff", billing_address: "fff", email: "fff", name: "fff")
+      @order.valid?.must_equal false
+      @order.errors.messages.must_include :CVV
+    end
+    it "CVV must be 3 digits" do
+      @order = Order.new(status: "paid", card_number: 2222222222222222, card_expiration: "10/12", CVV: 1234, billing_zip_code: 12345, shipping_address: "fff", billing_address: "fff", email: "fff", name: "fff")
+      @order.valid?.must_equal false
+      @order.errors.messages.must_include :CVV
+    end
+    it "billing_zip_code must be of type integer" do
+      @order = Order.new(status: "paid", card_number: 2222222222222222, card_expiration: "10/12", CVV: 123, billing_zip_code: "string", shipping_address: "fff", billing_address: "fff", email: "fff", name: "fff")
+      @order.valid?.must_equal false
+      @order.errors.messages.must_include :billing_zip_code
+    end
+    it "billing_zip_code must 5 digits" do
+      @order = Order.new(status: "paid", card_number: 2222222222222222, card_expiration: "10/12", CVV: 123, billing_zip_code: 123456, shipping_address: "fff", billing_address: "fff", email: "fff", name: "fff")
+      @order.valid?.must_equal false
+      @order.errors.messages.must_include :billing_zip_code
+    end
+  end
+  describe 'add product method' do
+    it 'finds a cart entry given a product id and order id' do
+      current_product = Order.add_product(products(:avocadotoast), orders(:persons_order).id)
+      current_product.must_equal cart_entries(:entry)
+    end
+    it 'returns current_product of type cart_entry' do
+      current_product = Order.add_product(products(:swisscheeseplant), orders(:persons_order).id)
+      current_product.must_be_kind_of CartEntry
+    end
+    it 'increments the cart entry quantity' do
+      order = Order.first
+      Order.add_product(products(:avocadotoast), orders(:persons_order).id)
+      order.cart_entries.count.must_equal 1
     end
   end
 end
-
-#   create_table "orders", force: :cascade do |t|
-#     t.datetime "created_at", null: false
-#     t.datetime "updated_at", null: false
-#     t.bigint "card_number"
-#     t.string "card_expiration"
-#     t.integer "CVV"
-#     t.integer "billing_zip_code"
-#     t.string "shipping_address"
-#     t.string "email"
-#     t.string "billing_address"
-#     t.string "status", default: "pending"
-#     t.string "name"
-#   end
-#
-#
-# describe CartEntry do
-#   describe "validations" do
-#     it "is valid when all fields are present" do
-#       @user = User.first
-#       @order = Order.new
-#       @order.save!
-#       @product = Product.new(name: 'Artisanal Coffee', category: "Lifestyle", quantity: 4, price: 9.99, image: 'https://placekitten.com/200/200', user: @user)
-#       @product.save!
-#       cart_entry = CartEntry.new(order: @order, order_id: @order.id, product: @product, product_id: @product.id)
-#       cart_entry.valid?.must_equal true
-#     end
-#
-#     it "requires an order" do
-#       cart_entry = CartEntry.new
-#       cart_entry.valid?.must_equal false
-#       cart_entry.errors.messages.must_include :order
-#     end
-#
-#     it "requires a product" do
-#       order_id = 1
-#       cart_entry = CartEntry.new(order_id: order_id)
-#       cart_entry.valid?.must_equal false
-#       cart_entry.errors.messages.must_include :product
-#     end
-#
-#     it "requires a quantity" do
-#       order_id = 1
-#       product_id = 1
-#       cart_entry = CartEntry.new(order_id: order_id, product_id: product_id)
-#       cart_entry.quantity = nil
-#       cart_entry.valid?.must_equal false
-#       cart_entry.errors.messages.must_include :quantity
-#     end
-#
-#     it 'the quantity must be of type integer' do
-#       @order = Order.new
-#       @order.save!
-#       product_id = 1
-#       cart_entry = CartEntry.new(order: @order, order_id: @order.id, product_id: product_id, quantity: 'string')
-#       cart_entry.valid?.must_equal false
-#       cart_entry.errors.messages.must_include :quantity
-#     end
-#
-#     it 'quantity must be greater than zero' do
-#       @order = Order.new
-#       @order.save!
-#       product_id = 1
-#       cart_entry = CartEntry.new(order: @order, order_id: @order.id, product_id: product_id, quantity: 0)
-#       cart_entry.valid?.must_equal false
-#       cart_entry.errors.messages.must_include :quantity
-#     end
-#
-#     it 'quantity cannot be a negative number' do
-#       @order = Order.new
-#       @order.save!
-#       product_id = 1
-#       cart_entry = CartEntry.new(order: @order, order_id: @order.id, product_id: product_id, quantity: -1)
-#       cart_entry.valid?.must_equal false
-#       cart_entry.errors.messages.must_include :quantity
-#     end
-#   end
-#
-#   describe 'relations' do
-#     it 'has an order' do
-#       cart_entry = cart_entries(:entry)
-#       cart_entry.order.must_equal orders(:persons_order)
-#     end
-#
-#     it "can set the order" do
-#       cart_entry = CartEntry.new
-#       cart_entry.order = orders(:persons_order)
-#       cart_entry.order_id.must_equal orders(:persons_order).id
-#     end
-#
-#     it 'has a product' do
-#       cart_entry = cart_entries(:entry)
-#       cart_entry.product.must_equal products(:avocadotoast)
-#     end
-#
-#     it "can set the product" do
-#       cart_entry = CartEntry.new
-#       cart_entry.product = products(:avocadotoast)
-#       cart_entry.product_id.must_equal products(:avocadotoast).id
-#     end
-#
-#   end
-# end
-#
-#
-# has_many :cart_entries, dependent: :destroy
-# validates :card_number, :presence, if: Proc.new { |a| a.status == "paid" }
-# validates :card_expiration, :presence, if: Proc.new { |a| a.status == "paid" }
-# validates :CVV, :presence, if: Proc.new { |a| a.status == "paid" }
-# validates :billing_address, :presence, if: Proc.new { |a| a.status == "paid" }
-# validates :billing_zip_code, :presence, if: Proc.new { |a| a.status == "paid" }
-# validates :email, :presence, if: Proc.new { |a| a.status == "paid" }
-# validates :shipping_address, :presence, if: Proc.new { |a| a.status == "paid" }
