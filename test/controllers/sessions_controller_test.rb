b4ef7dd40dba9b45cd3a7d303157ee77b48ee3a4
Karis Kim <ouniiq@hotmail.com>
2018-10-26 06:33:08 -0700

    added sessions controller testing and passes but says expect nil is deprecated and needs to be assert_nil but test passes so....

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,67 @@
 require "test_helper"
 
 describe SessionsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "logging in" do
+    it "log in with github as an existing user" do
+      user = users(:fetchuser)
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new( mock_auth_hash( user ) )
+
+      get login_path(:github)
+
+      must_redirect_to root_path
+      expect(session[:user_id]).must_equal user.id
+      expect(flash[:success]).must_equal "Get in, loser. We're going shopping. \u{1F485}"
+
+    end
+
+    it "creates a new user when logging in with valid data" do
+
+      start_count = User.count
+
+      new_user = User.new(username: "new user", uid: 3, provider: :github, email: 'fetch@fetch.com')
+
+      expect(new_user.valid?).must_equal true
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new( mock_auth_hash( new_user ) )
+
+      get login_path(:github)
+
+      must_redirect_to root_path
+      expect( User.count ).must_equal start_count + 1
+      expect( session[:user_id] ).must_equal User.last.id
+    end
+
+    it "does not create a new user when logging in with  invalid data" do
+      start_count = User.count
+
+      invalid_new_user = User.new(username: nil, email: nil)
+
+      expect(invalid_new_user.valid?).must_equal false
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new( mock_auth_hash( invalid_new_user ) )
+
+      get login_path(:github)
+
+      must_redirect_to root_path
+      expect( session[:user_id] ).must_equal nil
+      expect( User.count ).must_equal start_count
+    end
+  end
+
+  describe "logging out" do
+    it "log out with github as an existing user" do
+      user = users(:fetchuser)
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new( mock_auth_hash( user ) )
+
+      get login_path(:github)
+      delete logout_path(:github)
+
+
+      must_redirect_to root_path
+      expect(session[:user_id]).must_equal nil
+      expect(flash[:success]).must_equal "You have been logged out. Been there. Shopped that. \u{1F485}"
+    end
+  end
 end
